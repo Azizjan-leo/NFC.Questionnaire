e5b56f5d8144a9a2639ebef944b600d5c095da13
paradigmajs <bakhautdinov.adam@gmail.com>
2020-09-06 14:29:06 +0600

    create auth azs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
+  import React from 'react';
 
-import './App.css';
-import Questions from './components/Questions/Questions';
+  import './App.css';
+  import Questions from './components/Questions/Questions';
 
 
-function App() {
-  return (
-    <div className="App">
-      <Questions/>
-    </div>
-  );
-}
+  function App() {
+    return (
+      <div className="App">
+        <Questions/>
+      </div>
+    );
+  }
 
-export default App;
+  export default App;
--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -1,38 +1,78 @@
 import React, {useState, useEffect}from "react"
 import "../Questions.css"
+import Auth from "../Auth/Auth"
 
 function Questions(){
     const [data] = useState(["Заехали к нам, когда стрелка была уже «на нуле»?",
                                     "Заглянули за стаканчиком вашего любимого кофе?",
                                     "Оператор был вежлив и улыбчив?",
-                                    "Хорошо помыли авто на мойке?"])
+                                    "Хорошо помыли авто на мойке?",
+                                    "Спасибо за ваши ответы"])
     const [count, setCount] = useState(0)
     const [oneQuestion, setOnequsetion] = useState('')
+    const [show, setShow] = useState(false)
+    const [auth, setAuth] = useState(false)
+    const [message, setMessage] = useState('')
     useEffect(()=>{
-        if(count<data.length){
+        if(count==data.length-1){
             setOnequsetion(data[count])
-        }else{
-            alert("спасибо за ответы")
+            setShow(true)
+        }else if(count<data.length){
+            setOnequsetion(data[count])
         }
-        
+        else{
+            setCount(0)
+            setShow(false)
+        }
     })
+
+    const logins = (val)=>{
+        if(val.length < 8){
+                setMessage("ваш ID меньше 8")
+        }else{
+            // fetch('URL_GOES_HERE', { 
+            //     method: 'post', 
+            //     headers: new Headers({
+            //       'Authorization': 'Basic', 
+            //       'Content-Type': 'application/x-www-form-urlencoded'
+            //     }), 
+            //     body: 'Azs:'+val
+            //   });
+            setAuth(true)
+        }
+    }
     return(
-        <div className = 'main'>
+        <div>
+           {auth &&
+        
+            <div className = 'main'>
             <div className="container m-auto item-center
                 bg-color animate__animated animate__backInDown">
                     <div className="content m-auto">
                         <div className="answer text-center">
                             <p>{oneQuestion}</p>
                         </div>
-                        <div className="answers">
-                        <button onClick={()=>setCount(count+1)}>Нет</button>
-                        <button onClick={()=>setCount(count+1)}>Да</button>
                         
-                        </div>
+                        {!show  &&
+                            <div className="answers">
+                                <button onClick={()=>setCount(count+1)}>Нет</button>
+                                <button onClick={()=>setCount(count+1)}>Да</button>
+                            </div>
+                            
+                        }{show &&
+                            <div className="answers">
+                                <button onClick={()=>setCount(count+1)}>OK</button>
+                            </div>
+                        }
+                        
                         
                   </div>
             </div>
         </div>
+        } 
+        {!auth && <Auth funcLogin={logins} message={message} setMessage={setMessage}/> }
+        </div>
+        
 
     )
 }
